f69ffecc04c0ce90c89f1782a656fb03e876c1cb
togi06 <t_bayter_06@hotmail.com>
2023-01-15 13:51:02 +0100

    new adds

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -22,6 +22,13 @@ public class BrowserUtils {
 
         }
     }
+    /*
+    This method accepts 3 arguments
+    Arg1: webdriver
+    Arg2: expectedInUrl: for verify if the url contains given String
+        -If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
 
 
     public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl,String expectedInTitle ){
