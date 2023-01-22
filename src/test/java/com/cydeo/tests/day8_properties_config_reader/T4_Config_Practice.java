3c1a9b9a136bd09234e23b895e1293084d95136d
togi06 <t_bayter_06@hotmail.com>
2023-01-22 12:31:37 +0100

    new adds

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -1,45 +1,43 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class T4_Config_Practice {
 
-    public WebDriver driver;
+  // public WebDriver driver;
 
-    @BeforeMethod
-    public void setupMethod(){
+  //  @BeforeMethod
+  //  public void setupMethod(){
 
-        //We are getting browserType dynamically from our configuration.properties file
-        String browserType = ConfigurationReader.getProperty("browser");
+  //      //We are getting browserType dynamically from our configuration.properties file
+  //      String browserType = ConfigurationReader.getProperty("browser");
 
 
-        driver= WebDriverFactory.getDriver(browserType);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("https://google.com");
+  //    driver= WebDriverFactory.getDriver(browserType);
+  //   driver.manage().window().maximize();
+  //  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-    }
+  //  }
 
     @Test
     public void google_search_test() throws InterruptedException {
+        //Driver.getDriver() -->
 
-    //3- Write “apple” in search box
+        Driver.getDriver().get("https://google.com");
 
-        WebElement cookies = driver.findElement(By.xpath("//div[.='Tümünü kabul et']"));
-       cookies.click();
+        //3- Write “apple” in search box
+
+        WebElement cookies = Driver.getDriver().findElement(By.xpath("//div[.='Tümünü kabul et']"));
+        cookies.click();
         Thread.sleep(1000);
-        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue")+ Keys.ENTER);
 
 
@@ -48,7 +46,7 @@ public class T4_Config_Practice {
     //Expected: apple - Google Search
         String  expectedTitle = ConfigurationReader.getProperty("searchValue")+" - Google'da Ara";
                                 //configuration bölümünde ne yazarsan buradan ona ulaşacak
-        String actualTitle = driver.getTitle();
+        String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualTitle,expectedTitle);
 
