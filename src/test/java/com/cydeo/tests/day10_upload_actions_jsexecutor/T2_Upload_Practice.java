55863b4e3bc01823abf5174952f2e8f1ad8c8ae0
togi06 <t_bayter_06@hotmail.com>
2023-01-24 15:49:42 +0100

    new adds

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T2_Upload_Practice {
+
+    @Test
+    public void uphold_test(){
+        //1. Go to https://practice.cydeo.com/upload
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+
+        //2. Find some small file from your computer,
+        // and get the path of it.
+        String path = "C:/Users/t_bay/OneDrive/Desktop/SublimeText";
+
+
+        //3. Upload the file.
+        WebElement fileUphold = Driver.getDriver().findElement(By.id("file-upload"));
+        fileUphold.sendKeys(path);
+
+
+        WebElement uploadBtn = Driver.getDriver().findElement(By.id("file-submit"));
+        uploadBtn.click();
+
+        //4. Assert:
+        //-File uploaded text is displayed on the page
+
+    }
+
+
+
+
+}
