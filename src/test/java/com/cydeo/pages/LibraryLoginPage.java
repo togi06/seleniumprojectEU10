24da7fad3bc5ff83ec0198e4d1f728a4617e0063
togi06 <t_bayter_06@hotmail.com>
2023-02-06 12:52:58 +0100

    new adds

---
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -0,0 +1,35 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLoginPage {
+
+
+    //1-initialize the driver instance and object of the class
+    public LibraryLoginPage(){
+
+        /*
+        initElements method will create connection in between
+        the current driver session(instance) and the object the current class.
+
+         */
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    //2-use @FindBy annotation to locate web elements
+    @FindBy(xpath = "//input[@id='inputEmail']")
+    public WebElement inputUsername;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[.='Sing in']")
+    public WebElement singInButton;
+
+
+
+}
